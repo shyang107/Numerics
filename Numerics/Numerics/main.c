d0ec9d55967a1712c059f82275409c27dbf9b478
YANG, Shuh-Hua <shyang107@gmail.com>
2015-03-18 13:10:22 +0800

    Initialize

---
+++ Numerics/Numerics/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Numerics
+//
+//  Created by 楊樹華 on 2015/3/18.
+//  Copyright (c) 2015年 楊樹華. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
